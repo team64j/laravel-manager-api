34c3acd3791fe6d720fda22cbee8cacda24f2f19
Serg <64j@mail.ru>
2024-04-23 09:23:30 +0300

    up

--- src/Http/Controllers/TemplateController.php
+++ src/Http/Controllers/TemplateController.php
@@ -80,7 +80,7 @@ class TemplateController extends Controller
             ->paginate(Config::get('global.number_of_results'))
             ->appends($request->all());
 
-        $viewPath = Config::get('view.app');
+        $viewPath = Config::get('view.app', resource_path('views'));
         $viewRelativePath = str_replace([base_path(), DIRECTORY_SEPARATOR], ['', '/'], resource_path('views'));
 
         $callbackItem = function (SiteTemplate $item) use ($viewPath, $viewRelativePath) {
